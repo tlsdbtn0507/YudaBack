186291339b1e2a65871e5e9efd101a206b9ee356
yusuShin <tlsdbtn0507@naver.com>
2024-12-27 22:15:51 +0900

    style(w-module) Days Enum 수정

--- back/src/configs/weathers.model.ts
+++ back/src/configs/weathers.model.ts
@@ -1,15 +1,15 @@
 export interface Weathers {
-  temp: number,
-  rain: number,
-  main: string,
+  temp: number;
+  rain: number;
+  main: string;
 }
 
 export enum Days {
-  SUN = 'sun',
-  MON = 'mon',
-  TUE = 'tue', 
-  WED = 'wed',
-  THU = 'thu',
-  FRI = 'fri',
-  SAT = 'sat',
+  SUN = "Sun",
+  MON = "Mon",
+  TUE = "Tue",
+  WED = "Wed",
+  THU = "Thu",
+  FRI = "Fri",
+  SAT = "Sat",
 }
