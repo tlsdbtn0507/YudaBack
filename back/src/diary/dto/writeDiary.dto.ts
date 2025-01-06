e706bf1f4bb10b2bb39060dd5fa089a4e775f979
yusuShin <tlsdbtn0507@naver.com>
2025-01-06 21:00:18 +0900

    feat(diary)일기의 요일을 구분하기 위해 일기의entity와 dto에 key dayOfWeek 추가

--- back/src/diary/dto/writeDiary.dto.ts
+++ back/src/diary/dto/writeDiary.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, isNumber, IsNumber, IsObject, IsOptional, IsString, Matches, MaxLength } from "class-validator";
+import { IsNotEmpty, IsObject, Matches, MaxLength } from "class-validator";
 import { Days } from "src/configs/weathers.model";
 
 export class WriteDiaryDTO {
@@ -21,28 +21,22 @@ export class WriteDiaryDTO {
 
   @IsNotEmpty()
   @MaxLength(300, {
-    message: "내용 제한 초과"
+    message: "내용 제한 초과",
   })
   feelingReason: string;
-  
+
   @IsNotEmpty()
   @Matches(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/, {
-    message: '날짜 형식 오류'
+    message: "날짜 형식 오류",
   })
   diaryDate: string;
 
-  // @Matches(/^(-?\d+(\.\d{1,6})?)$/, {
-  //   message: 'Latitude must be a number with up to 6 decimal places',
-  // })
-  // @IsFlo()
-  //   @IsOptional()
+  @IsNotEmpty()
+  dayOfWeek: Days;
+
+  @IsNotEmpty()
   lat: string;
 
-  // @Matches(/^(-?\d+(\.\d{1,6})?)$/, {
-  //   message: 'Longitude must be a number with up to 6 decimal places',
-  // })
-  // @IsNumber()
-  //   @IsOptional()
+  @IsNotEmpty()
   long: string;
-
 }
